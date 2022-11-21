2f4b4789d4aec7b9f6dc6e85f8fb5886752b6790
Uijin <abrahamkim98@gmail.com>
2022-11-21 09:47:47 +0000

    test

--- faster_rcnn_swin.py
+++ faster_rcnn_swin.py
@@ -4,7 +4,7 @@ _base_=[
     './_base_/schedules/schedule_1x.py',
     './_base_/default_runtime.py'
 ]
-
+# zzz
 pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
 model = dict(
     type='MaskRCNN',
