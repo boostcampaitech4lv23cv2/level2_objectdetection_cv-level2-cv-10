5727c89eac5e6e1f25b3b0efb6b5d997bcb5e4af
Uijin <abrahamkim98@gmail.com>
2022-11-23 06:15:48 +0000

    DetectoRs

--- _base_/default_runtime.py
+++ _base_/default_runtime.py
@@ -10,7 +10,7 @@ log_config = dict(
          init_kwargs={
             'project': 'mmdetection',
             'entity': 'godkimyoungju',
-            'name': 'swin_fpn_lrcfg_loss'
+            'name': 'resnet_rfp'
             }
         )
     ])
